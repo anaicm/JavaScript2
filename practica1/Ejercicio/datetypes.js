5046e635bc4c223b3bbfac811304a7e7f8113042
Ana Isabel Cuéllar Maestro <100219176+anaicm@users.noreply.github.com>
2023-07-13 12:09:01 +0200

    repasando

---
+++ practica1/Ejercicio/datetypes.js
@@ -0,0 +1,9 @@
+let nombre="Ana";//String
+let casado=true;//Booleano
+let valor=null;//nulo valor vacío/nulo
+let pais;//Indefinido, si no se asigna un valor a una variable es indefinido. Además, si una función no devuelve nada, devuelve undefined.
+
+console.log(typeof "ana");
+console.log(typeof 7);
+console.log(typeof true);
+console.log(typeof undefined);
---
+++ practica1/Ejercicio/variable.js
@@ -0,0 +1,4 @@
+let nombre="Ana";//String
+let casado=true;//Booleano
+let valor=null;//nulo valor vacío/nulo
+let pais;//Indefinido, si no se asigna un valor a una variable es indefinido. Además, si una función no devuelve nada, devuelve undefined.
